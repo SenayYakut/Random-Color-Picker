1bbe1d69253ce90b12a466b6ff29975b5f2d3590
Senay Yakut <senaykt@gmail.com>
2021-04-04 22:21:41 -0700

    Add constructor to the random

--- Random.js
+++ Random.js
@@ -2,19 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Random extends React.Component {
-  componentDidMount() {
-    this.applyColor();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: [255, 181, 255]
+        };
+    }
+    componentDidMount() {
+        this.applyColor();
+    }
 
-  componentDidUpdate(prevProps, prevState) {
-    this.applyColor();
-  }
+    componentDidUpdate(prevProps, prevState) {
+        this.applyColor();
+    }
 
-  formatColor(ary) {
-    return 'rgb(' + ary.join(', ') + ')';
-  }
+    formatColor(ary) {
+        return 'rgb(' + ary.join(', ') + ')';
+    }
 
-  isLight() {
-    const rgb = this.state.color;
-    return rgb.reduce((a,b) => a+b) < 127 * 3;
-  }
+    isLight() {
+        const rgb = this.state.color;
+        return rgb.reduce((a, b) => a + b) < 127 * 3;
+    }
+
+    applyColor() {
+        const color = this.formatColor(this.state.color);
+        document.body.style.background = color;
+    }
+
+    chooseColor() {
+        const random = [];
+        for (let i = 0; i < 3; i++) {
+            random.push(Math.floor(Math.random() * 256));
+        }
+        return random;
+    }
+
+    render() {
+        return ( <
+            div >
+            <
+            h1 className = { this.isLight() ? 'white' : 'black' } >
+            Your color is { this.formatColor(this.state.color) }!
+            <
+            /h1> <
+            /div>
+        );
+    }
+}
+
+ReactDOM.render( <
+    Random / > ,
+    document.getElementById('app')
+);
